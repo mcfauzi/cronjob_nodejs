69d0c9987e453cdefc39ffdb80384d0285944fc0
M.C. Fauzi <sanzcruzer@gmail.com>
2019-06-14 19:03:04 +0700

    fix format crontab

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import cron from "node-cron";
 import processData from "./process";
 
-cron.schedule("* * * * *", () => {
+cron.schedule("01 00 * * *", () => {
     //cron job every midnight day at 00:01
     console.log(`---STARTING CRON JOB EVERY DAY AT 00:01---`);
     processData();
