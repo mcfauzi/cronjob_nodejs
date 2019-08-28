6b46726167a766a333375d67af5cebadda3a082f
M.C. Fauzi <sanzcruzer@gmail.com>
2019-08-28 10:52:58 +0700

    lorem ipsum

--- libs/db.js
+++ libs/db.js
@@ -4,11 +4,19 @@ const mysql = require('mysql');
 // for testing
 // const db = mysql.createConnection({
 const db = mysql.createPool({
-    host: 'YOUR_HOST',
-    user: 'YOUR_USER',
-    password: 'YOUR_PASS',
-    database: 'YOUR_DB',
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'gotravel_dev',
 });
+
+// for live
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'gotravel_admin',
+//     password: 'GoTravelly0102',
+//     database: 'gotravel_live',
+// });
 
 // export default db;
 module.exports = {
